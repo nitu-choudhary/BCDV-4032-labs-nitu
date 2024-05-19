65cfe9103c4754e196d0ab409c5b61e258642116
Nitu Choudhary <learningadventureswithnitu@gmail.com>
2024-05-19 10:47:42 -0400

    Adding Example React App code

---
+++ Lab1/example-app-section2/docker-react-example/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Hi, Developer!</h2>
+        <p>
+          Basic React App Example for Docker Lab 1
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
